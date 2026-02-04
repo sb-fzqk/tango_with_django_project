fb29a16b8d54872c3988a8792aa7db3b07b445c8
Svyatoslav Boychuk <3026407b@student.gla.ac.uk>
2026-02-04 13:02:02 +0000

    Finished Chapter 4 Exercises and Passed the Test

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -22,6 +22,7 @@ from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
